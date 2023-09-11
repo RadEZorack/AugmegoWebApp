3e3dac8df5f16e50c674d584cc22f8e94c826764
Travis Miller <radezorack@gmail.com>
2023-09-11 15:49:49 -0400

    install nvm

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -xe
 
+# Download Node version manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+
+# Test
+node -e "console.log('Running Node.js ' + process.version)"
+
 # Start Node, the application server.
-systemctl yum install npm
-systemctl npm run start
+npm run start
