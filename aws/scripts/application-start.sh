9196486dac9fad993f3aa7751ab17b72d1ded559
Travis Miller <radezorack@gmail.com>
2023-09-11 16:02:47 -0400

    nvm install 16

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -4,7 +4,7 @@ set -xe
 # Download Node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install --lts
+nvm install 16
 
 # Test
 node -e "console.log('Running Node.js ' + process.version)"
