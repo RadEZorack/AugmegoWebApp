468e7b0320b98520f3605ce1e65344b14e2ce99e
Travis Miller <radezorack@gmail.com>
2023-09-11 17:10:44 -0400

    again

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -4,7 +4,7 @@ set -xe
 
 # Copy war file from S3 bucket to tomcat webapp folder
 mkdir /usr/local/node/
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-303x914k3ow9/augmego-web-app.tar /usr/local/node/augmego-web-app.tar
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-303x914k3ow9/augmego-web-app.tar /usr/local/node/augmego-web-app.tar.gz
 
 # Ensure the ownership permissions are correct.
 # chown -R tomcat:tomcat /usr/local/tomcat9/webapps
